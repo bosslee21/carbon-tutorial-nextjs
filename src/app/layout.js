5866480cdef8a12ff26fd95be891cc6f6c9ea297
Jae Butler <jaebutler@jaes-air.mynetworksettings.com>
2024-12-11 12:49:25 -0500

    feat(tutorial): complete step2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,23 @@
-import './globals.scss'
+// server-side layout
+// Equivalent to App.tsx in your other project, but it’s more modular
+// because layouts in Next.js 13 are hierarchical and scoped to
+// specific parts of your app.
+
+//
+import './globals.scss';
 import { Providers } from './providers';
 
 export const metadata = {
   title: 'Carbon + Next13',
   description: 'IBM Carbon Tutorial with NextJS 13',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
